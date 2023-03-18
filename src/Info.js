a10f28cdfa9e83d743e35a264b2cdab49da0c539
Mario <lintt74123@gmail.com>
2023-03-18 15:11:28 +0800

    JSX practice-1

--- src/Info.js
+++ src/Info.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const Info = () => {
-  return (
-    <div><h1>This is Info component.</h1></div>
-  )
-}
+    let friends = ["Mario", "Maria", "Max"];
+    let test = [1, 2, 3, 4];
+    const aaa = () => {
+        console.log("Hello");
+        return 100000;
+    };
+    return (
+        <div>
+            <h1>This is Info component.</h1>
+            <h1>{1234567890}</h1>
+            <h2>{77 * 66}</h2>
+            <h3>{Math.random()}</h3>
+            <h4>{aaa()}</h4>
+            <p>My friends are :</p>
+            {friends.map((friend) => {
+                return <p>{friend}</p>;
+            })}
+            <p>{test}</p>
+            {friends.map((friend) => (
+                <p>My friend is {friend}</p>
+            ))}
+        </div>
+    );
+};
 
-export default Info
+export default Info;
