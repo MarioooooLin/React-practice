b2e5e56a97b5c6dd6a7fe50ef9ac448e61e60871
Mario <lintt74123@gmail.com>
2023-03-18 22:37:45 +0800

    useEffect practice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 //import Nav from "./Nav";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Info from "./Info";
 import Create from "./Create";
 
 function App() {
-    let [msg, setMsg] = useState([]);
+    let [myName, setMyName] = useState("Mario Lin");
+
+    const changeBtn = () => {
+        setMyName("Maria Lee");
+    };
+
+    useEffect(() => {
+        console.log("useEffect's function is running");
+    }, [myName]);
+
+    // let [msg, setMsg] = useState([]);
 
     // let friends = ["A", "B", "C", "D"];
     // let test1 = [
@@ -21,8 +31,10 @@ function App() {
     return (
         <div>
             <h1>This is h1 tag</h1>
+            <h2>{myName}</h2>
+            <button onClick={changeBtn}>Change</button>
             {/* <Nav /> */}
-            <Create msg={msg} setMsg={setMsg} />
+            {/* <Create msg={msg} setMsg={setMsg} /> */}
             {/* <Info />
             <Info name={friends[0]} age={15} />
             <Info name={friends[1]} age={20} />
@@ -46,7 +58,7 @@ function App() {
                 test 3
             </button>
             <button onClick={clickButton}>SIMPLE</button> */}
-            <Info msg={msg} setMsg={setMsg} />
+            {/* <Info msg={msg} setMsg={setMsg} /> */}
         </div>
     );
 }
